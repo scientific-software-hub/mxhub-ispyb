0878e89beacd75a15050bdd6770cf23c7aa2a4b0
olga <olga.merkulova@desy.de>
2024-12-19 12:28:04 +0100

    Revert "Revert "Fix entityManager.merge does not set null value (#25)""
    This reverts commit 5a5f60f580aad5d786ed14fe8f4f2991bf120dc5.

--- ispyb-ejb/src/test/java/ispyb/server/common/services/shipping/Container3ServiceBeanTest.java
+++ ispyb-ejb/src/test/java/ispyb/server/common/services/shipping/Container3ServiceBeanTest.java
@@ -6,6 +6,7 @@ import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 public class Container3ServiceBeanTest extends TestBase {
 
@@ -17,6 +18,16 @@ public class Container3ServiceBeanTest extends TestBase {
     public void findByPk() throws Exception {
         var result = service.findByPk(13, false);
         assertNotNull(result);
+    }
+
+    @Ignore("Requires db to be up and running")
+    @Test
+    public void testUpdate_setNull() throws Exception {
+        var vo = service.findByPk(13, false);
+        vo.setSampleChangerLocation(null);
+        var result = service.update(vo);
+        assertNotNull(result);
+        assertNull(result.getSampleChangerLocation());
     }
 
 }
